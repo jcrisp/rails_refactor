c63a6f57908d12f6d6760dce6fdedde16f85cc45
James Crisp <james@Macel.local>
2010-11-24 21:40:01 +1100

    update view

--- lib/rails_refactor.rb
+++ lib/rails_refactor.rb
@@ -4,47 +4,66 @@
 
 require 'config/environment'
 
-def replace_in_file(path, find, replace)
-  contents = File.read(path)
-  contents.gsub!(find, replace)
-  File.open(path, "w+") { |f| f.write(contents) }
-end
+class Renamer
+  def initialize(from, to)
+    @from, @to = from, to
 
-def controller_rename(from, to)
-  to_controller_path = "app/controllers/#{to.underscore}.rb"
-  from_resource_name = from.gsub(/Controller$/, "")
-  to_resource_name   = to.gsub(/Controller$/, "")
+    @from_controller, @from_action = from.split(".")
 
-  from_resource_path = from_resource_name.underscore
-  to_resource_path   = to_resource_name.underscore
+    @from_resource_name = @from_controller.gsub(/Controller$/, "")
+    @to_resource_name   = to.gsub(/Controller$/, "")
 
-  `mv app/controllers/#{from.underscore}.rb #{to_controller_path}`
-  replace_in_file(to_controller_path, from, to)
+    @from_resource_path = @from_resource_name.underscore
 
-  `mv app/views/#{from_resource_path} app/views/#{to_resource_path}`
+    @to_resource_path   = @to_resource_name.underscore
+  end
 
-  to_helper_path = "app/helpers/#{to_resource_path}_helper.rb"
-  `mv app/helpers/#{from_resource_path}_helper.rb #{to_helper_path}`
+  def replace_in_file(path, find, replace)
+    contents = File.read(path)
+    contents.gsub!(find, replace)
+    File.open(path, "w+") { |f| f.write(contents) }
+  end
 
-  replace_in_file(to_helper_path, from_resource_name, to_resource_name)
+  def controller_rename
+    @to_controller_path = "app/controllers/#{@to.underscore}.rb"
 
-  replace_in_file('config/routes.rb', from_resource_path, to_resource_path)
-end
+    `mv app/controllers/#{@from.underscore}.rb #{@to_controller_path}`
+    replace_in_file(@to_controller_path, @from, @to)
 
-def controller_action_rename(from, to)
-  controller, action = from.split('.')
-  controller_path = "app/controllers/#{controller.underscore}.rb"
-  p controller_path
-  replace_in_file(controller_path, action, to)
+    `mv app/views/#{@from_resource_path} app/views/#{@to_resource_path}`
+
+    @to_helper_path = "app/helpers/#{@to_resource_path}_helper.rb"
+    `mv app/helpers/#{@from_resource_path}_helper.rb #{@to_helper_path}`
+
+    replace_in_file(@to_helper_path, @from_resource_name, @to_resource_name)
+
+    replace_in_file('config/routes.rb', @from_resource_path, @to_resource_path)
+  end
+
+  def controller_action_rename
+    controller, action = @from.split('.')
+    controller_path = "app/controllers/#{controller.underscore}.rb"
+    replace_in_file(controller_path, action, @to)
+    
+    views_for_action = "app/views/#{@from_resource_path}/#{action}.*"
+
+    Dir[views_for_action].each do |file|
+      extension = file.split('.')[1..2].join('.')
+      cmd = "mv #{file} app/views/#{@from_resource_path}/#{@to}.#{extension}"
+      `#{cmd}`
+    end
+  end
 end
 
 if ARGV.length == 3
   command, from, to = ARGV
+  renamer = Renamer.new(from, to)
+
   if command == "rename"
     if from.include? '.'
-      controller_action_rename(from, to)
+      renamer.controller_action_rename 
     else
-      controller_rename(from, to)
+      rename.controller_rename
     end
   end
 else
@@ -56,6 +75,18 @@ else
       `rm -rf app/views/hello_world`
     end
 
+    def renamer(from, to)
+      Renamer.new(from, to)
+    end
+
+    def controller_action_rename(from, to)
+      renamer(from, to).controller_action_rename
+    end
+
+    def controller_rename(from, to)
+      renamer(from, to).controller_rename
+    end
+
     def assert_file_changed(path, from, to)
       contents = File.read(path)
       assert contents.include?(to) 
@@ -65,6 +96,8 @@ else
     def test_controller_action_rename
       controller_action_rename('DummiesController.index', 'new_action')
       assert_file_changed("app/controllers/dummies_controller.rb", "index", "new_action")
+      assert File.exists?("app/views/dummies/new_action.html.erb")
+      assert !File.exists?("app/views/dummies/index.html.erb")
     end
 
     def test_controller_rename
