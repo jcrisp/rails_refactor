f979a152d4bc1dce41a6a5b59f755d277dd73e29
James Crisp <james@Macel.local>
2010-11-24 19:23:54 +1100

    Views

--- dummy/app/controllers/dummies_controller.rb
+++ dummy/app/controllers/dummies_controller.rb
@@ -1,2 +1,5 @@
 class DummiesController < ApplicationController
+  def index
+  end
+
 end
--- dummy/test/functional/dummies_controller_test.rb
+++ dummy/test/functional/dummies_controller_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class DummiesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get index" do
+    get :index
+    assert_response :success
   end
+
 end
