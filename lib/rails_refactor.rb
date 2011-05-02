4f63f660a700669e472fda6edd2530c29ce2d4d0
James Crisp <james@Macel.local>
2011-05-02 11:51:17 +1000

    Handle loading rails_refactor when not in rails project

--- lib/rails_refactor.rb
+++ lib/rails_refactor.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 
-require './config/environment.rb'
+begin
+  require './config/environment.rb'
+rescue LoadError
+  puts "*** rails_refactor needs to be run from the root of a Rails 3 webapp ***"
+  exit
+end
 
 class Renamer
   def initialize(from, to)
